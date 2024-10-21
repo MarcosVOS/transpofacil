e69adbf6dd103139529ae065f7d0c821c8ad5f72
MarcosVOS <marcos.oliveiraDEV@gmail.com>
2024-10-20 21:48:49 -0300

    style: adjust layout for content centering and image responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,33 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div>
+      <main className="flex flex-col items-center justify-center min-h-screen p-4">
         <Image
             src="/images/logo.png"
             alt="Transpofacil logo"
             width={180}    
             height={200}
-            layout="responsive"  
+            className="w-[180px] h-auto" 
             priority
             placeholder="blur"
             blurDataURL="data:image/svg+xml;base64,..."
         />
+        <form action=""  className="mt-4 flex flex-col gap-2" >
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" id="email" placeholder="Digite seu e-mail" autoComplete="email"/>
+          <Label htmlFor="password">Senha</Label>
+          <Input type="password" id="password" placeholder="Digite sua senha" autoComplete="current-password"/>
+          <Button type="submit">Entrar</Button>
+        </form>
+        
       </main>
-      <footer className={styles.footer}>
+      <footer>
       </footer>
     </div>
   );
