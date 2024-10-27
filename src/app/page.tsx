0170725e52e64996bdea03d088a35dcf76d9d895
MarcosVOS <marcos.oliveiraDEV@gmail.com>
2024-10-27 17:18:01 -0300

    feat: update button size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,23 +9,32 @@ export default function Home() {
     <div>
       <main className="flex flex-col items-center justify-center min-h-screen p-4">
         <Image
-            src="/images/logo.png"
-            alt="Transpofacil logo"
-            width={180}    
-            height={200}
-            className="w-[180px] h-auto" 
-            priority
-            placeholder="blur"
-            blurDataURL="data:image/svg+xml;base64,..."
+          src="/images/logo.png"
+          alt="Transpofacil logo"
+          width={180}
+          height={200}
+          className="w-auto h-auto"
+          priority
+          placeholder="blur"
+          blurDataURL="data:image/svg+xml;base64,..."
         />
-        <form action=""  className="mt-4 flex flex-col gap-2" >
+        <form action="" className="mt-4 flex flex-col gap-2" >
           <Label htmlFor="email">Email</Label>
-          <Input type="email" id="email" placeholder="Digite seu e-mail" autoComplete="email"/>
+          <Input type="email"
+            id="email"
+            placeholder="Digite seu e-mail"
+            autoComplete="email"
+          />
           <Label htmlFor="password">Senha</Label>
-          <Input type="password" id="password" placeholder="Digite sua senha" autoComplete="current-password"/>
+          <Input type="password"
+            id="password"
+            placeholder="Digite sua senha"
+            autoComplete="current-password" 
+            className="w-80"
+          />
           <Button type="submit" variant="green">Entrar</Button>
         </form>
-        
+
       </main>
       <footer>
       </footer>
