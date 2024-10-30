a9efae04a99d5660367a90fde67ae30e34714f53
MarcosVOS <marcos.oliveiraDEV@gmail.com>
2024-10-30 16:08:59 -0300

    test: maps

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,20 @@
-export default function teste() {
-  return <div>Auth</div>;
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
+export default async function Page() {
+  const Map = useMemo(() => dynamic(
+    () => import('@/components/map'),
+    {
+      loading: () => <p>A map is loading</p>,
+      ssr: false
+    }
+  ), [])
+
+  return (
+    <>
+      <div className="bg-white-700 mx-auto my-5 w-[98%] h-[480px]">
+        <Map posix={[-23.5582869, -46.6620722]} />
+      </div>
+    </>
+  )
 }
