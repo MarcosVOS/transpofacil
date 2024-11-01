83c38ef8628a03c0090201cec5837209a553893e
MarcosVOS <marcos.oliveiraDEV@gmail.com>
2024-10-31 21:55:16 -0300

    feat: add Leaflet map component and display on home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,21 @@
-export default function teste() {
-  return <div>Auth</div>;
+import dynamic from "next/dynamic"
+import { useMemo } from "react";
+
+
+export default function home() {
+  const Map = useMemo(() => dynamic(
+    () => import('@/components/map/'),
+    {
+      loading: () => <p>A map is loading</p>,
+      ssr: false
+    }
+  ), [])
+
+  return (
+    <div className="w-[100vw] h-[100vh]">
+      <div className="bg-white-700 mx-auto w-[100%] h-[100%]">
+        <Map posix={[4.79029, -75.69003]} />
+      </div>
+    </div>
+  )
 }
